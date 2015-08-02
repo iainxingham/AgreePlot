d7d430fa71ef2d355b6cb09c6e4a50f72bfa7400
Iain Crossingham <irc91914@gmail.com>
2015-08-02 22:20:00 +0100

    Added data parameter to BAplot

--- R/BA.r
+++ R/BA.r
@@ -3,7 +3,7 @@
 
 #' Bland Altman plot
 #' 
-#' \code{BAplot} draws a Bland Altman plot given a set of paired measurements
+#' \code{BAplot} draws a Bland Altman plot
 #' 
 #' <Need to fill in details here>
 #' 
@@ -12,8 +12,10 @@
 #'   to draw the limits of agreement. Defaults to 1.96
 #' @param subject A vector identifying the subject for replicates. NULL if only 
 #'   one pair of measures per subject
-#' @param measure.method A vector identifying the method used for x. Ignored 
+#' @param measure.method A factor vector identifying the method used for x. Ignored 
 #'   if y defined
+#' @param data An optional dataframe containing x, y, subject and/or 
+#'   measure.method  
 #' @param xlab x axis label
 #' @param ylab y axis label
 #' @param main Plot title
@@ -26,18 +28,26 @@
 BAplot <- function(x, y=NULL, limit=1.96,
                    subject=NULL,
                    measure.method=NULL,
+                   data=NULL,
                    xlab="Average of x & y",
                    ylab="Difference, x-y",
                    main="Bland Altman plot",
                    ...) {
   
+  # If provided a dataframe then use this
+  x <- eval(substitute(x), data, parent.frame())
+  y <- eval(substitute(y), data, parent.frame())
+  subject <- eval(substitute(subject), data, parent.frame())
+  measure.method <- eval(substitute(measure.method), data, parent.frame())
+  
   # If subject isn't defined then need pairs of measurements in x and y
-  if(subject==NULL) {
-    # Need to add if measure.method is defined
-    if(y==NULL) stop("Need either paired x & y or for subject to be defined")
-    if(length(x) != length(y)) stop("Need paired measurements in x and y")
-    
-    # Paired x and y, no replicates
+  if(is.null(subject)) {
+    if(is.null(y))
+      stop("Need either paired x & y or for subject to be defined")
+      
+    # Paired x and y, no replicates    
+    if(length(x) != length(y)) stop("Need paired measurements in x and y 
+                                    if subject not defined")
     LA <- mean(x-y, na.rm=TRUE) + c(0-limit, limit)*sd(x-y, na.rm=TRUE)  
     plot((x+y)/2, (x-y), type="p", xlab=xlab, ylab=ylab, 
          main=main, ...)
@@ -45,8 +55,33 @@ BAplot <- function(x, y=NULL, limit=1.96,
     abline(h=LA[1])
     abline(h=LA[2])
   }
+    
+  # Subject defined
   else {
-    # Vector in x with subject 
+    # Different methods in x and y
+    if(!is.null(y)) {
+      #<to do>
+    }
+    
+    # x only, method defined
+    else {
+      if(length(x) != length(measure.method)) 
+        stop("x and measure.method should be equal length")
+      if(length(x) != length(subject))
+        stop("x and subject should be the same lenght")
+      if(!is.factor(measure.method)) stop("measure.method should be a factor")
+      if(nlevels(measure.method) < 2) stop("measure.method should have 
+                                           at least two levels")
+      
+      # Could have bit here allowing user to specify levels from measure.method
+      methlevels <- levels(measure.method)
+      tempdf <- data.frame(x, subject, measure.method)
+      # <to do>
+      # Now cycle through subject and calculate 
+      #   mean x per subject
+      #   mean x per subject & measure.method == methlevels[1]
+      #   mean x per subject & measure.method == methlevels[2]
+    }  
   }
 
 }
