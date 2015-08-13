e19edec30d355d15fd8e3e9b84b897558a7dd224
Iain Crossingham <irc91914@gmail.com>
2015-08-13 19:37:37 +0100

    Fixed bugs in BAplot(), replicated pairs

--- R/BA.r
+++ R/BA.r
@@ -7,15 +7,15 @@
 #' 
 #' <Need to fill in details here>
 #' 
-#' @param x,y Two numeric vectors containing paired measurements
+#' @param x,y Either two numeric vectors containing paired measurements made with
+#'   different methods or a single numeric vector x with y=NULL and method defined.
 #' @param limit Number of standard deviations each side of \code{mean(x-y)}
-#'   to draw the limits of agreement. Defaults to 1.96
-#' @param subject A vector identifying the subject for replicates. NULL if only 
-#'   one pair of measures per subject
-#' @param measure.method A factor vector identifying the method used for x. Ignored 
-#'   if y defined
-#' @param data An optional dataframe containing x, y, subject and/or 
-#'   measure.method  
+#'   to draw the limits of agreement. Defaults to 1.96.
+#' @param subject A factor vector identifying the subject for replicates. 
+#'   NULL if only one pair of measures per subject.
+#' @param method A factor vector identifying the method used for x. Ignored 
+#'   if y defined.
+#' @param data An optional dataframe containing x, y, subject and/or method  
 #' @param xlab x axis label
 #' @param ylab y axis label
 #' @param main Plot title
@@ -27,7 +27,7 @@
 #' 
 BAplot <- function(x, y=NULL, limit=1.96,
                    subject=NULL,
-                   measure.method=NULL,
+                   method=NULL,
                    data=NULL,
                    xlab="Average of x & y",
                    ylab="Difference, x-y",
@@ -38,7 +38,7 @@ BAplot <- function(x, y=NULL, limit=1.96,
   x <- eval(substitute(x), data, parent.frame())
   y <- eval(substitute(y), data, parent.frame())
   subject <- eval(substitute(subject), data, parent.frame())
-  measure.method <- eval(substitute(measure.method), data, parent.frame())
+  method <- eval(substitute(method), data, parent.frame())
   
   # If subject isn't defined then need pairs of measurements in x and y
   if(is.null(subject)) {
@@ -66,7 +66,7 @@ BAplot <- function(x, y=NULL, limit=1.96,
         stop("Subject and x vectors should be equal length")
       if(!is.factor(subject)) stop("Subject should be a factor")
       
-      tempdf <- data.frame(x, y, subject, diff=(x-y), avg=(x+y)/2)
+      tempdf <- data.frame(x, y, subject, diff=(x-y))
       anova_result <- anova(lm(diff~subject, data = tempdf))
       
       # Using dplyr for this - replace with aggregate()??
@@ -74,19 +74,14 @@ BAplot <- function(x, y=NULL, limit=1.96,
         #group_by(subject) %>%
         #summarise(avg=mean(diff), stddev=sd(diff), num=n())
       
-      # Calculate mean differences to plot
-      avgdiff <- aggregate(tempdf["diff"], by=tempdf["subject"], FUN=mean)
-      avgread <- aggregate(tempdf["avg"], by=tempdf["subject"], FUN=mean)
-      
       # Calculate sd for limits of agreement
-      # <to do> Check handling of zeros here
       mi <- aggregate(tempdf["diff"], by=tempdf["subject"], FUN=length)
-      divisor <- (sum(mi[2])^2 - sum(mi[2]^2)) / ((nrow(mi)-1) * sum(mi[2]))
+      divisor <- (sum(mi[,2])^2 - sum(mi[,2]^2)) / ((nrow(mi)-1) * sum(mi[,2]))
       stdev <- sqrt(anova_result[2,3] + 
                       ((anova_result[1,3] - anova_result[2,3])/divisor))
-      LA <- mean(avgdiff) + c(0-limit, limit) * stdev 
+      LA <- mean(x-y) + c(0-limit, limit) * stdev 
       
-      plot(avgread[2], avgdiff[2], type="p", xlab=xlab, ylab=ylab, 
+      plot((x+y)/2, x-y, type="p", xlab=xlab, ylab=ylab, 
            main=main, ...)
       abline(h=0, lty=2)
       abline(h=LA[1])
@@ -95,17 +90,16 @@ BAplot <- function(x, y=NULL, limit=1.96,
     
     # x only, method defined
     else {
-      if(length(x) != length(measure.method)) 
-        stop("x and measure.method should be equal length")
+      if(length(x) != length(method)) 
+        stop("x and method should be equal length")
       if(length(x) != length(subject))
         stop("x and subject should be the same lenght")
-      if(!is.factor(measure.method)) stop("measure.method should be a factor")
-      if(nlevels(measure.method) < 2) stop("measure.method should have 
-                                           at least two levels")
+      if(!is.factor(method)) stop("method should be a factor")
+      if(nlevels(method) < 2) stop("method should have at least two levels")
       
       # Could have bit here allowing user to specify levels from measure.method
-      methlevels <- levels(measure.method)
-      tempdf <- data.frame(x, subject, measure.method)
+      methlevels <- levels(method)
+      tempdf <- data.frame(x, subject, method)
       # <to do>
       # Now cycle through subject and calculate 
       #   mean x per subject
